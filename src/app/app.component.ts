7317ea9b6ea3ed047d10716f60a3bc4c13ccb10e
Leonardo Da Re <leonardo.dare@dbagroup.it>
2025-07-02 16:07:40 +0200

    lesson 37

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,18 +2,24 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from "./header/header.component";
 import { UserComponent } from "./user/user.component";
 import { DUMMY_USERS } from "./dummy-users";
+import { TasksComponent } from "./tasks/tasks.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, UserComponent],
+  imports: [HeaderComponent, UserComponent, TasksComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   users = DUMMY_USERS; 
+  selectedUserId = 'Select a user to show tasks';
 
   onSelectUser(id: string) {
-    console.log('Selected user ID:', id);
+    console.log(id);
+  }
+
+  onSelectUser2(name: string) {
+    this.selectedUserId = name;
   }
 }
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input, Output, EventEmitter, input, output } from '@angular/core';
 
 @Component({
   selector: 'app-user',
@@ -13,13 +13,23 @@ export class UserComponent {
   @Input({ required: true }) id!: string;
   @Input({ required: true }) avatar!: string;
   @Input({ required: true }) name!: string;
-  @Output() select = new EventEmitter();
+  @Output() select = new EventEmitter<string>();  // type specification is not required as we can emit any type of data, but specifying will ensure that only what we want will work
+
+  @Output() select2 = new EventEmitter<string>();
+
+
+  // with signals
+  /* id = input.required<string>();
+  avatar = input.required<string>();
+  name = input.required<string>();
+  select = output<string>();  */ // (new function) output doesnt use signals, but is a usefuls alternative to EventEmitter if we dont want any decorators
 
   get imagePath() {
     return 'assets/users/' + this.avatar;
   }
 
   onSelectUser() {
-    this.select.emit(this.id);
+    this.select.emit(this.id); 
+    this.select2.emit(this.name);
   }
 }
